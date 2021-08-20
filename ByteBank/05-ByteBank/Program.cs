1dd194606822a556104fd5629a7f01ca9241f6f9
anoyori <anoyori@gmail.com>
2021-08-19 22:59:56 -0300

    Namespaces e tratamento de null

---
+++ ByteBank/05-ByteBank/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cliente gabriela = new Cliente();
+
+            //gabriela.nome = "Gabriela";
+            //gabriela.profissao = "Desenvolvedora C#";
+            //gabriela.cpf = "000.000.000-00";
+
+            //conta.titular = gabriela;
+            ContaCorrente conta = new ContaCorrente();
+            conta.titular = new Cliente();
+            conta.titular.nome = "Gabriela Costa";
+            conta.titular.cpf = "000.000.000-00";
+            conta.titular.profissao = "Desenvolvedora C#";
+
+            conta.saldo = 500;
+            conta.agencia = 123;
+            conta.numeroDaConta = 1233455;
+
+            //Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular.nome);
+            Console.WriteLine(conta.titular.cpf);
+            Console.WriteLine(conta.titular.profissao);
+
+            Console.ReadLine();
+        }
+    }
+}
